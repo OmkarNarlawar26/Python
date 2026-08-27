b5e67a8ca631e282e2aa151f81e04e074fd1d8af
Omkar <omkarnarlawar26@gmail.com>
2026-08-28 00:32:27 +0530

    Updated

--- 17_PONG-THE_FAMOUS_ARCADE_GAME/Main.py
+++ 17_PONG-THE_FAMOUS_ARCADE_GAME/Main.py
@@ -21,8 +21,12 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.01)
+    time.sleep(0.1)
     screen.update()
     ball.move()
+
+    # detect collision on wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 screen.exitonclick()
