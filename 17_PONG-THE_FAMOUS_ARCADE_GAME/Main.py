85a640ca902ae319e6b66bd70d87e7035930896d
Omkar <omkarnarlawar26@gmail.com>
2026-08-28 02:52:47 +0530

    Added

--- 17_PONG-THE_FAMOUS_ARCADE_GAME/Main.py
+++ 17_PONG-THE_FAMOUS_ARCADE_GAME/Main.py
@@ -16,14 +16,18 @@ ball = Ball()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+
+# Right paddle
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+
+# Left paddle
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,15 +36,25 @@ while game_is_on:
         ball.bounce_y()
 
     # detect collision with paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
         ball.bounce_x()
 
     # detect when R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # detect when L paddle misses
-    if ball.xcor() > -380:
+    if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
+
+    # check is someone reacher 5 points
+    if scoreboard.l_score == 5:
+        game_is_on = False
+        scoreboard.game_over("LEFT PLAYER")
+    elif scoreboard.r_score == 5:
+        game_is_on = False
+        scoreboard.game_over("RIGHT PLAYER")
 
 screen.exitonclick()
--- 17_PONG-THE_FAMOUS_ARCADE_GAME/scoreboard.py
+++ 17_PONG-THE_FAMOUS_ARCADE_GAME/scoreboard.py
@@ -9,10 +9,23 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 60, "normal"))
         self.goto(100, 200)
         self.write(self.r_score, align="center", font=("Courier", 60, "normal"))
 
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
 
-    
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+    def game_over(self, winner):
+        self.goto(0, 0)
+        self.write(f"{winner} WINS!", align="center", font=("Courier", 40, "bold"))
